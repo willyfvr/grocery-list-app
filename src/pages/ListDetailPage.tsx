6c3460487fd56424f22d613f43ef81c167bed87a
Victor Fattor <victor.fattor@bosonic.digital>
2026-06-23 14:28:56 -0300

    feat: change ux when the status is purchased

--- src/pages/ListDetailPage.tsx
+++ src/pages/ListDetailPage.tsx
@@ -1,25 +1,30 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { CreateItemForm } from "../components/shopping/CreateItemForm";
+import { ITEM_STATUS } from "../constants/item-status";
 import { useItems } from "../hooks/useItems";
 import { AppLayout } from "../layouts/AppLayout";
 import { shoppingListService } from "../services/shopping-list.service";
-import { CreateItemForm } from "../components/shopping/CreateItemForm";
-import { getStatusLabel} from "../utils/item-status";
+import { getStatusLabel } from "../utils/item-status";
 
 export function ListDetailPage() {
+  const [showPurchased, setShowPurchased] = useState(true);
+
   const { id } = useParams();
 
-  console.log("ID URL: ", id);
-  /* TODO: this line should be delete  */
-  const lists = shoppingListService.getAll();
-  console.log("LISTAS: ", lists);
+  // const lists = shoppingListService.getAll();
 
   const list = id ? shoppingListService.getById(id) : undefined;
 
-  const { 
-    items, 
-    createItem,
-    updateItemStatus
-   } = useItems(list?.id ?? "");
+  const { items, createItem, updateItemStatus } = useItems(list?.id ?? "");
+
+  console.log("showPurchased: ", showPurchased);
+
+  const visibleItems = showPurchased
+    ? items
+    : items.filter((item) => item.status !== ITEM_STATUS.PURCHASED);
+
+  console.log("visibleItems:", visibleItems);
 
   if (!list) {
     return (
@@ -38,25 +43,38 @@ export function ListDetailPage() {
       </div>
 
       <CreateItemForm onCreate={createItem} />
-      
+
       <p>Cantidad de items: {items.length} </p>
 
+      <label className="flex items-center gap-2 mt-4">
+        <input
+          type="checkbox"
+          checked={showPurchased}
+          onChange={(e) => setShowPurchased(e.target.checked)}
+        />
+        Mostrar productos comprados
+      </label>
+
       <div className="mt-4">
-        {items.map((item) => (
-          <div key={item.id} className="border rounded p-2 mt-2">
-            <div>
-              {item.name}
-            </div>
-            
-            {/* <div>
-              {getStatusLabel(item.status)}
-            </div> */}
+        {visibleItems.map((item) => {
+          const isPurchased = item.status === ITEM_STATUS.PURCHASED;
+
+          return (
+          <div
+            key={item.id}
+            className={`
+            border rounded p-2 mt-2 
+            ${isPurchased ? "opacity-50" : ""}
+          `}
+          >
+            <div
+              className={isPurchased ? "line-through" : ""}>{item.name}</div>
 
             <button onClick={() => updateItemStatus(item.id)}>
               {getStatusLabel(item.status)}
             </button>
           </div>
-        ))}
+        )})}
       </div>
     </AppLayout>
   );
